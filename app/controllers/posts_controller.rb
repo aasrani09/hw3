5f9d60889c0fdedb9fc82661202fa521ba3fbd7d
aasrani09 <aarti.asrani@kellogg.northwestern.edu>
2022-05-19 20:07:39 +0000

    vf

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,4 @@
 class PostsController < ApplicationController
-  def index
-      @posts = Post.all
-    end
-  
-    def show
-      @post = Post.find_by({ "id" => params["id"]})
-      @place = Place.find_by({ "id" => @post["place_id"] })
-    end
   
     def new 
     @post = Post.new
